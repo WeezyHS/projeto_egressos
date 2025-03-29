59acc159230b4f834668501d7ba1b77e5da39652
wesley.henderson <wesley.henderson@rede.ulbra.br>
2025-03-29 17:12:49 -0300

    Alterações na página de login para conta de Instituição!

--- app/login_instituicao/page.tsx
+++ app/login_instituicao/page.tsx
@@ -1,35 +1,46 @@
 'use client';
 
+import styles from './login_instituicao.module.css';
+
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 
 export default function CriarContaInstituicao() {
-  const [email, setEmail] = useState('');
-  const [senha, setSenha] = useState('');
-  const [erro, setErro] = useState(''); //Mensagem de erro
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
   const router = useRouter();
 
-  const handleSubmit = (event: React.FormEvent) => {
-    event.preventDefault();
-    //Adicionar a lógica para enviar os dados para o backend
-    console.log('Email:', email, 'Senha:', senha);
-
-    router.push("/perfil_instituicao"); //Redireciona para perfilinstituicao
+  //Caso os campos não sejam preenchidos
+  const camposVazios = () =>{
+    if (!email || !senha){
+      alert("Preencha todos os campos antes de continuar!");
+      return false;
+    }
+    return true;
   };
 
+  const handleCriarConta = () =>{
+    if (!camposVazios()) return;
+
+    console.log('Email:', email, 'Senha:', senha);
+    router.push("/perfil_instituicao"); //Redireciona para perfilinstituicao
+  }
+  const handleEntrar = () =>{
+    if (!camposVazios()) return;
+    router.push("/app_instituicao");
+  }
+
   return (
-    <div>
-      <h1>Conta de Instituição</h1>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="email">Email:</label>
-        <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
-        <label htmlFor="senha">Senha:</label>
-        <input type="password" id="senha" value={senha} onChange={(e) => setSenha(e.target.value)}/>
+    <div className={styles.container}>
+      <h1 className={styles.titulo}>Conta de Instituição</h1>
 
-        <button type="submit">Criar Conta</button>
+      <label htmlFor="email">Email:</label>
+      <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+      <label htmlFor="senha">Senha:</label>
+      <input type="password" id="senha" value={senha} onChange={(e) => setSenha(e.target.value)}/>
 
-      </form>
-      <button onClick={() => router.push("/app_instituicao")}>Entrar</button><br/>
+      <button onClick={handleCriarConta}>Criar Conta</button><br/>
+      <button onClick={handleEntrar}>Entrar</button>
     </div>
   );
 }
