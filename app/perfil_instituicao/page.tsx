f0ceceda279f5fab3934b89c065c8823894c3029
wesley.henderson <wesley.henderson@rede.ulbra.br>
2025-03-31 02:10:09 -0300

    Adição de rota e validação de campos vazios da página!

--- app/perfil_instituicao/page.tsx
+++ app/perfil_instituicao/page.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { useRouter } from "next/navigation";
+import styles from "./perfil_instituicao.module.css";
 import { useState } from 'react';
 
 export default function PerfilInstituicao() {
@@ -12,9 +14,20 @@ export default function PerfilInstituicao() {
   const [nomeRepresentante, setNomeRepresentante] = useState('');
   const [cpfRepresentante, setCpfRepresentante] = useState('');
 
+  const router = useRouter();
+
+  const camposVazios = () =>{
+    if (!fotoPerfil || !nomeCompleto || !cnpj || !telefone || !endereco || !cep || !nomeRepresentante || !cpfRepresentante){
+      alert("Preencha todos os campos antes de continuar!");
+      return false;
+    }
+    return true;
+  }
+
   const handleSubmit = (event: React.FormEvent) => {
+    if (!camposVazios()) return;
     event.preventDefault();
-    // Aqui você pode adicionar a lógica para enviar os dados para o backend
+    // Adicionar a lógica para enviar os dados para o backend
     console.log(
       'Foto do Perfil:', fotoPerfil,
       'Nome Completo:', nomeCompleto,
@@ -28,40 +41,41 @@ export default function PerfilInstituicao() {
   };
 
   const handleFotoPerfilChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (!camposVazios()) return;
     if (event.target.files && event.target.files[0]) {
       setFotoPerfil(event.target.files[0]);
     }
   };
 
   return (
-    <div>
+    <div className={styles.container}>
       <h1>Perfil da Instituição</h1>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="fotoPerfil" className="fotoPerfil">Foto do Perfil:</label>
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <label htmlFor="fotoPerfil" className={styles.labPerfil}>Foto do Perfil:</label>
         <input type="file" id="fotoPerfil" accept="image/*" onChange={handleFotoPerfilChange} />
 
-        <label htmlFor="nomeCompleto">Nome Completo:</label>
-        <input type="text" id="nomeCompleto" value={nomeCompleto} onChange={(e) => setNomeCompleto(e.target.value)} />
+        <label className={styles.labelPadrao} htmlFor="nomeCompleto">Nome Completo:</label>
+        <input className={styles.inputPadrao} type="text" id="nomeCompleto" value={nomeCompleto} onChange={(e) => setNomeCompleto(e.target.value)} />
 
-        <label htmlFor="cnpj">CNPJ:</label>
-        <input type="text" id="cnpj" value={cnpj} onChange={(e) => setCnpj(e.target.value)} />
+        <label className={styles.labelPadrao} htmlFor="cnpj">CNPJ:</label>
+        <input className={styles.inputPadrao} type="text" id="cnpj" value={cnpj} onChange={(e) => setCnpj(e.target.value)} />
 
-        <label htmlFor="telefone">Telefone:</label>
-        <input type="tel" id="telefone" value={telefone} onChange={(e) => setTelefone(e.target.value)} />
+        <label className={styles.labelPadrao} htmlFor="telefone">Telefone:</label>
+        <input className={styles.inputPadrao} type="tel" id="telefone" value={telefone} onChange={(e) => setTelefone(e.target.value)} />
 
-        <label htmlFor="endereco">Endereço:</label>
-        <input type="text" id="endereco" value={endereco} onChange={(e) => setEndereco(e.target.value)} />
+        <label className={styles.labelPadrao} htmlFor="endereco">Endereço:</label>
+        <input className={styles.inputPadrao} type="text" id="endereco" value={endereco} onChange={(e) => setEndereco(e.target.value)} />
 
-        <label htmlFor="cep">CEP:</label>
-        <input type="text" id="cep" value={cep} onChange={(e) => setCep(e.target.value)} />
+        <label className={styles.labelPadrao} htmlFor="cep">CEP:</label>
+        <input className={styles.inputPadrao} type="text" id="cep" value={cep} onChange={(e) => setCep(e.target.value)} />
 
-        <label htmlFor="nomeRepresentante">Nome do Representante Legal:</label>
-        <input type="text" id="nomeRepresentante" value={nomeRepresentante} onChange={(e) => setNomeRepresentante(e.target.value)} />
+        <label className={styles.labelPadrao} htmlFor="nomeRepresentante">Nome do Representante Legal:</label>
+        <input className={styles.inputPadrao} type="text" id="nomeRepresentante" value={nomeRepresentante} onChange={(e) => setNomeRepresentante(e.target.value)} />
 
-        <label htmlFor="cpfRepresentante">CPF do Representante Legal:</label>
-        <input type="text" id="cpfRepresentante" value={cpfRepresentante} onChange={(e) => setCpfRepresentante(e.target.value)} />
+        <label className={styles.labelPadrao} htmlFor="cpfRepresentante">CPF do Representante Legal:</label>
+        <input className={styles.inputPadrao} type="text" id="cpfRepresentante" value={cpfRepresentante} onChange={(e) => setCpfRepresentante(e.target.value)} />
 
-        <button type="submit">Salvar Perfil</button>
+        <button className={styles.salvarPerfil} onClick={() => router.push("/app_instituicao")}>Salvar Perfil</button>
       </form>
     </div>
   );
