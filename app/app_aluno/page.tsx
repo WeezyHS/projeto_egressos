43d07991c32a05ba262449e86df9ab5b570bdf80
wesley.henderson <wesley.henderson@rede.ulbra.br>
2025-04-04 18:01:58 -0300

    Criação da "carcaça" da página "app_aluno"!

---
+++ app/app_aluno/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import styles from './app_aluno.module.css';
+import { useState } from 'react';
+import { useRouter } from 'next/router';
+
+export default function App_Aluno(){
+    return(
+        <div>
+            <h1 className={styles.titulo}>AINDA PARA DESENVOLVER...</h1>
+        </div>
+    );
+}
