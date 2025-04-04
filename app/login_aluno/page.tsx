b836a1a4d1bbb3985ce81ffa4668a877b1dd821d
wesley.henderson <wesley.henderson@rede.ulbra.br>
2025-04-04 17:58:55 -0300

    Criação da página "app_aluno"!

--- app/login_aluno/page.tsx
+++ app/login_aluno/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import styles from './login_aluno.module.css';
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 
@@ -8,26 +9,47 @@ export default function CriarContaInstituicao() {
   const [senha, setSenha] = useState('');
   const router = useRouter();
 
-  const handleSubmit = (event: React.FormEvent) => {
-    event.preventDefault();
-    // Adicionar a lógica para enviar os dados para o backend
-    console.log('Email:', email, 'Senha:', senha);
-
-    router.push("/perfil_instituicao"); //Redireciona para perfilinstituicao
+  const validacaoEmail = (email: string) => {
+    const regex = /^[^\s@]+@(gmail\.com|outlook\.com)$/;
+    return regex.test(email);
+  }
+  //Caso os campos não sejam preenchidos e caso o e-mail seja inválido
+  const camposVazios = () =>{
+    if (!email || !senha){
+      alert("Preencha todos os campos antes de continuar!");
+      return false;
+    }
+    if (!validacaoEmail(email)){
+      alert("Insira um e-mail válido!");
+    }
+    if (senha.length < 8){
+      alert("A senha deve ter no mínimo 8 caracteres!");
+      return false;
+    }
+    return true;
   };
 
+  const handleCriarConta = () =>{ //Direcionta para perfil_aluno
+    if (!camposVazios()) return;
+
+    router.push("");
+  }
+
+  const handleEntrar = () => { //Direciona para app_aluno
+    if (!camposVazios()) return;
+    router.push("/app_aluno");
+  }
+
   return (
-    <div>
-      <h1>Criar Conta de Usuário</h1>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="email">Email:</label>
-        <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
-        <label htmlFor="senha">Senha:</label>
-        <input type="password" id="senha" value={senha} onChange={(e) => setSenha(e.target.value)}/>
+    <div className={styles.container}>
+      <h1 className={styles.titulo}>Conta de Usuário</h1>
+        <label className={styles.labEmail} htmlFor="email">Email:</label>
+        <input className={styles.email} type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+        <label className={styles.labSenha} htmlFor="senha">Senha:</label>
+        <input className={styles.senha} type="password" id="senha" value={senha} onChange={(e) => setSenha(e.target.value)}/>
 
-        <button type="submit">Criar Conta</button>
-
-      </form>
+        <button className={styles.CriarConta} onClick={handleCriarConta}>Criar Conta</button>
+        <button className={styles.Entrar} onClick={handleEntrar}>Entrar</button>
     </div>
   );
 }
