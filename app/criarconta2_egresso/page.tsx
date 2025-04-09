223d1a80bd29ac76326f90d312e709b379d2cbad
wesley.henderson <wesley.henderson@rede.ulbra.br>
2025-04-09 15:59:15 -0300

    Criação da 2° página, "criarconta2_egresso"!

--- app/criarconta2_egresso/page.tsx
+++ app/criarconta2_egresso/page.tsx
@@ -4,7 +4,26 @@ import styles from './criarconta2_egresso.module.css';
 import { useState } from 'react';
 
 export default function CriarContaEgresso2(){
+    const [empresa, setEmpresa] = useState('');
+    const [cidadeEmpresa, setCidadeEmpresa] = useState('');
+    const [estadoEmpresa, setEstadoEmpresa] = useState('');
+    const [paisEmpresa, setPaisEmpresa] = useState('');
+    const [cargo, setCargo] = useState('');
+    const [anoEntrada, setAnoEntrada] = useState('');
+
+    const handleCadastrar = () =>{
+        if (!empresa || !cidadeEmpresa || !estadoEmpresa || !paisEmpresa || !cargo || !anoEntrada) {
+            alert('Preencha todos os campos!');
+            return;
+        }
+        alert("Informações salvas!");
+    }
     return(
-        <h1>PARA DESENVOLVER</h1>
+        <div className={styles.container}>
+        <h1 className={styles.titulo}>PARA DESENVOLVER</h1>
+        <div className={styles.duasColunas}>
+
+        </div>
+        </div>
     );
 }
