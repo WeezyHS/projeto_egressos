9bbfa5cce19a65932d26970c179dd1265541f720
wesley.henderson <wesley.henderson@rede.ulbra.br>
2025-04-10 00:52:31 -0300

    Adição de lista teste com filtragem de nome e cargo. Configuração das áreas de texto e input da página!

--- app/app_aluno/page.tsx
+++ app/app_aluno/page.tsx
@@ -1,13 +1,50 @@
 'use client';
 
 import styles from './app_aluno.module.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 
+interface Egresso{
+    id: number;
+    nome: string;
+    cargoAtual: string;
+}
+
 export default function App_Aluno(){
+    const [egressos, setEgressos] = useState<Egresso[]>([]);
+    const [filtroNome, setFiltroNome] = useState('');
+    const [filtroCargo, setFiltroCargo] = useState('');
+
+    useEffect(() =>{ //Simulação da lista de egressos com filtragem, ainda para desenvolver conectado ao banco de dados
+        const dadosSimulacao: Egresso[] = [
+            { id: 1, nome: 'João Silva', cargoAtual: 'Engenheiro de Software' },
+            { id: 2, nome: 'Maria Oliveira', cargoAtual: 'Analista de Dados' },
+            { id: 3, nome: 'Carlos Pereira', cargoAtual: 'Gerente de Projetos' }
+        ];
+        setEgressos(dadosSimulacao);
+    }, []);
+
+    const egressosFiltrados = egressos.filter((egresso) =>
+        egresso.nome.toLowerCase().includes(filtroNome.toLowerCase()) &&
+        egresso.cargoAtual.toLowerCase().includes(filtroCargo.toLowerCase())
+      );
+
     return(
-        <div>
-            <h1 className={styles.titulo}>AINDA PARA DESENVOLVER...</h1>
+        <div className={styles.container}>
+            <h1 className={styles.titulo}>DESENVOLVENDO...</h1>
+            <div className={styles.filtros}>
+              <label className={styles.labFiltroNome}>Pesquisar nome:</label>
+              <input className={styles.filtroNome} type="text" placeholder="Pesquisar por nome..." value={filtroNome} onChange={(e) => setFiltroNome(e.target.value)}/>
+              <label className={styles.labFiltroCargo}>Pesquisar cargo:</label>
+              <input className={styles.filtroCargo} type="text" placeholder="Pesquisar por cargo..." value={filtroCargo} onChange={(e) => setFiltroCargo(e.target.value)}/>
+            </div>
+            <ul className={styles.lista}>
+        {egressosFiltrados.map((egresso) => (
+          <li className={styles.item} key={egresso.id}>
+            <strong>{egresso.nome}</strong> — {egresso.cargoAtual}
+          </li>
+        ))}
+      </ul>
         </div>
     );
 }
