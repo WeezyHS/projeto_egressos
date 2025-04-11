940171808a7e6ed6e71673ea2386083099654444
wesley.henderson <wesley.henderson@rede.ulbra.br>
2025-04-11 12:19:51 -0300

    Alteração no botão da página!

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function EscolhaPerfil() {
       <h1 className="text-2xl font-bold mb-6">Escolha seu perfil</h1>
       <div className="space-y-4">
         <button className="px-6 py-3 bg-blue-500 text-white rounded-lg shadow-md hover:bg-blue-600 transition" onClick={() => router.push('/login_instituicao')}>Instituição</button><br/>
-        <button className="px-6 py-3 bg-green-500 text-white rounded-lg shadow-md hover:bg-green-600 transition" onClick={() => router.push('/login_aluno')}>Aluno</button>
+        <button className="px-6 py-3 bg-green-500 text-white rounded-lg shadow-md hover:bg-green-600 transition" onClick={() => router.push('/login_egresso')}>Aluno</button>
       </div>
     </div>
   );
