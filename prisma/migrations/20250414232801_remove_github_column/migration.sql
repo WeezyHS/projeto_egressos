1894ac08e8d5971d42c412af2211429232f4c978
wesley.henderson <wesley.henderson@rede.ulbra.br>
2025-04-14 22:01:03 -0300

    Exclusão da coluna "github" do banco de dados!

---
+++ prisma/migrations/20250414232801_remove_github_column/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `github` on the `egresso` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `egresso` DROP COLUMN `github`;
