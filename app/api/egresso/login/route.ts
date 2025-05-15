2c0c24df93eed29753e347fce95e6f0b736bd3a3
wesley.henderson <wesley.henderson@rede.ulbra.br>
2025-05-15 19:00:33 -0300

    Remoção de linhas desnecessárias e modificação de textos!

--- app/api/egresso/login/route.ts
+++ app/api/egresso/login/route.ts
@@ -7,7 +7,6 @@ import { NextResponse } from 'next/server';
 export async function POST(request: Request){
     try{
         const { email, senha } = await request.json();
-        // const senhaHash = await bcrypt.hash(senha, 10);
         if (!email || !senha) {
             return NextResponse.json({ error: "E-mail e senha são obrigatórios" }, { status: 400});
         }
@@ -18,13 +17,13 @@ export async function POST(request: Request){
         })
 
         if (!egresso){ //Caso não exista a conta no banco de dados
-            return NextResponse.json({ error: "Credenciais inexistentes!" }, {status: 401 });
+            return NextResponse.json({ error: "Conta inexistente!" }, {status: 401 });
         }
 
         const senhaCorreta = await bcrypt.compare(senha, egresso.senha);
 
         if (!senhaCorreta){
-            return NextResponse.json({ error: "Credenciais inválidas" }, { status: 401 });
+            return NextResponse.json({ error: "Credenciais inválidas!" }, { status: 401 });
         }
 
         const { senha: passwordHash, ...egressoSemSenha } = egresso;
@@ -32,8 +31,7 @@ export async function POST(request: Request){
         return NextResponse.json({ egresso: egressoSemSenha }, { status: 200 });
 
     }catch (error){
-        console.error("Erro durante o login:", error);
-        return NextResponse.json({ error: "Erro interno do servidor" }, { status: 500 });
+        return NextResponse.json({ error: "Erro interno do servidor!" }, { status: 500 });
     } finally{
         const prisma = new PrismaClient();
         await prisma.$disconnect();
